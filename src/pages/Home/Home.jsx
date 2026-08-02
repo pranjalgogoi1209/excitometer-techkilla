75da85de7dc8480bd9d629ddae7a015783648343
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2026-08-02 10:17:02 +0530

    add files

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,27 @@
+import Header from "./../../components/Header";
+import Header from "./../../components/Pillar";
+import Header from "./../../components/Meter";
+import Header from "./../../components/Wave";
+import Header from "./../../components/Mic";
+
 const Home = () => {
-  return <>Home Page</>;
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Pillar />
+
+        <Meter />
+
+        <Pillar />
+      </main>
+
+      <Wave />
+
+      <Mic />
+    </>
+  );
 };
 
 export default Home;
