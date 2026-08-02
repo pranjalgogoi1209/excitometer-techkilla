974f771500d8bc66534e022b9bab302bc2e6654d
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2026-08-02 11:28:51 +0530

    update assets

--- src/components/Meter/Meter.jsx
+++ src/components/Meter/Meter.jsx
@@ -1,5 +1,8 @@
 import meter from "../../assets/images/meter.png";
 
+import Wave from "../Wave/Wave";
+import Mic from "../Mic/Mic";
+
 import "./Meter.scss";
 
 const Meter = () => {
@@ -7,9 +10,17 @@ const Meter = () => {
     <section className="meter">
       <img src={meter} alt="Excitometer Meter" className="meter__image" />
 
-      {/* Needle will be added later */}
+      {/* Needle will be added here */}
 
-      {/* Score will be added later */}
+      {/* Score will be added here */}
+
+      <div className="meter__bottom">
+        <Wave />
+
+        <Mic />
+
+        <Wave />
+      </div>
     </section>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,6 @@
 import Header from "../../components/Header/Header";
 import Meter from "../../components/Meter/Meter";
 import Pillar from "../../components/Pillar/Pillar";
-import Wave from "../../components/Wave/Wave";
-import Mic from "../../components/Mic/Mic";
 
 import "./Home.scss";
 
@@ -18,14 +16,6 @@ const Home = () => {
 
         <Pillar side="right" />
       </div>
-
-      {/* <div className="bottom">
-        <Wave />
-
-        <Mic />
-
-        <Wave />
-      </div> */}
     </div>
   );
 };
